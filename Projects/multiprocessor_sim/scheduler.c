5adde84bc07fef001852a906396dfc934edaf2b6
abo7atm <aaalnumay@gmail.com>
2019-04-05 16:33:52 +0300

    Finish insert_ready_queue()

--- Projects/multiprocessor_sim/scheduler.c
+++ Projects/multiprocessor_sim/scheduler.c
@@ -17,7 +17,7 @@
  * waiting_queue: jobs blocked waitng for IO
  * */
 
-#define MAX_MEMORY = 2000;
+#define MAX_MEMORY 2000
 
 Process_queue *job_queue, *ready_queue, *waiting_queue;
 int burst = 10, current_memory_usage = 0, IO_load, CPU_load;
@@ -28,6 +28,7 @@ void initialize_pqs()
     ready_queue = init_pq();
     waiting_queue = init_pq();
 }
+
 // Probably no need for this function, as enqueue handles allocation memory for process_node
 // Process_node* init_process_node(Process *process)
 // {
@@ -48,25 +49,48 @@ void insert_job_queue(Process **processes, int n_processes)
     }
 }
 
-/* takes jobs from job_queue, insert them into memroy (ready_queue) */
-/* aka long term scheduler */
-void insert_ready_queue()
+/** takes jobs from job_queue, insert them into memroy (ready_queue)
+ * aka long term scheduler
+ * 
+ * Initial classification:
+ * if process requires more than one IO resource,
+ * it's considered an IO bound process,
+ * otherwise, it's a cpu bound process. */
+void insert_ready_queue(Process *process)
 {
-    /** categorize processes into either 1) CPU bound, or 2) IO bound
-     * initially, if process requires one IO resource,
-     * it'll be considred as IO bound
-     * */
+    int count_resources; /* count number of resources required */
+    int memory_needed = job_queue->head->data->mem;
+
     if (job_queue->size == 0)
     {
         printf("No jobs available\n");
         return;
     }
+    else if (current_memory_usage == MAX_MEMORY || memory_needed >= (MAX_MEMORY - current_memory_usage))
+    {
+        printf("No enough memeory for job\n");
+        return;
+    }
 
     Process_node *temp = job_queue->head;
+    Process *d = temp->data;
 
-    while(temp->next != NULL)
+    count_resources = d->res_a + d->res_b + d->res_c + d->res_d;
+
+    enqueue(ready_queue, process);
+    
+    current_memory_usage += process->mem;
+
+    if (count_resources > 1)
     {
-        
+        /** check IO_load, and compare to CPU_load,
+         * if process can be 
+         */
+        IO_load++;
+    }
+    else
+    {
+        CPU_load++;
     }
 }
 
